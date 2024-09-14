decaba14557e064b064f1aa66ca79da2c53f7c78
tylerjheinz <tylerjensenheinz@outlook.com>
2024-09-14 07:40:05 -0400

    Update site.pp

--- site.pp
+++ site.pp
@@ -1,3 +1,8 @@
 node default {
     include chocolatey
+    chocolateysource {'chocolatey':
+        ensure   => present,
+        location => 'https://chocolatey.org/api/v2',
+        priority => 1,
+    }
 }
